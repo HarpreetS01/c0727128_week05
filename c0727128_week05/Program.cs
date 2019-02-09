5c30721217845e8025305ced9335b9dc71f53f70
visualstudio <visualstudio@122-PC21>
2019-02-08 20:45:59 -0500

    commit 8:30

--- c0727128_week05/Program.cs
+++ c0727128_week05/Program.cs
@@ -8,38 +8,69 @@ namespace c0727128_week05
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("Hello");
+           
         }
+       
     }
 
     class Node
     {
         public Node() { }
-
-        public Node floor;
         public Node elevatorUp;
         public string FloorNumber;
     }
 
+    class Department
+    {
+        public Department aDepartment;
+        public string DepartmentDescription;
+
+    }
+
     class Elevator
     {
+        Node Head;
         Node FirstFloor;
         Node SecondFloor;
         Node ThirdFloor;
         Node FourthFloor;
 
-        public void run()
+        public void setup()
         {
             FirstFloor = new Node();
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
+            Head = FirstFloor;
             FirstFloor.FloorNumber = "First Floor";
-            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
             FirstFloor.elevatorUp = SecondFloor;
             SecondFloor.FloorNumber = "Second Floor";
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            FourthFloor.elevatorUp = null;
+        }
 
-            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
+        public void TraverseList()
+        {
+            Node temp;
+            temp = Head;
+
+            // where am I going to start?
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
+               
+
+            }
+
         }
     }
 }
